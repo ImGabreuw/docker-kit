faa97c009b615c6a35b1f5791f8957b95fdce13c
ImGabreuw <imgabreuw.dev@gmail.com>
2025-04-27 00:48:08 -0300

    feat: add build script for Docker image creation

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <path/to/Dockerfile>"
+  exit 1
+fi
+
+DOCKERFILE_PATH="$1"
+
+if [ ! -f "$DOCKERFILE_PATH" ]; then
+  echo "Arquivo Dockerfile não encontrado: $DOCKERFILE_PATH"
+  exit 2
+fi
+
+RELATIVE_PATH=$(dirname "$DOCKERFILE_PATH")
+STACK=$(echo "$RELATIVE_PATH" | sed 's#^/##' | sed 's#/$##' | tr '/' '-' | tr '[:upper:]' '[:lower:]')
+
+IMAGE_NAME="imgabreuw/docker-kit:$STACK"
+
+echo "Building image: $IMAGE_NAME"
+docker build -f "$DOCKERFILE_PATH" -t "$IMAGE_NAME" .
